299c44f47a39d437fbe75178cb27519b9b805f5f
Claire Moore <nrmoore@uci.edu>
2021-11-30 22:53:00 -0800

    Day 1 Solution
    Kotlin with functional list constructs

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,24 @@
+import java.lang.Integer.parseInt
+
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        return input.map(::parseInt).zipWithNext().count { (first, second) ->
+            first < second
+        }
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        return input.map(::parseInt).windowed(3, 1) { window ->
+            window.sum()
+        }.zipWithNext().count { (firstSum, secondSum) ->
+            firstSum < secondSum
+        }
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 7)
+    check(part2(testInput) == 5)
 
     val input = readInput("Day01")
     println(part1(input))
