78135a480e91d37c15731f6ea4e07fe1d18e60db
Claire Moore <nrmoore@uci.edu>
2021-12-01 23:23:21 -0800

    Add in Day 2 C Solution
    Difficult to debug, memory issues still exist to track down, but the
    algorithms themsleves work and are correct in C now.

--- c/include/lib.h
+++ c/include/lib.h
@@ -3,6 +3,8 @@
 #include <ctype.h>
 #include <string.h>
 #include <stdlib.h>
+#include <stdio.h>
+#include <errno.h>
 
 char* get_test_input_filename(char *args[]) {
     char *progname = args[0];
@@ -32,6 +34,66 @@ char* get_input_filename(char *args[]) {
     strncat(filename, ".txt", 5);
     filename[8] = toupper(filename[8]);
     return filename;
+}
+
+void free_str_array(char** array) {
+    if (array != NULL) {
+        char** cleanup_helper = array;
+        for (char** cleanup_helper = array; !*cleanup_helper; cleanup_helper++) {
+            free(*cleanup_helper);
+        }
+        free(array);
+    }
+}
+
+char** load_strs(FILE* file) {
+    int array_cap = 5;
+    int array_len = 0;
+    size_t str_size = 0;
+    char** array = malloc(array_cap * sizeof(char*));
+    if (array == NULL) {
+        return NULL;
+    }
+
+    int old_errno = errno;
+    errno = 0;
+    do {
+        if (array_len == array_cap) {
+            array_cap *= 2;
+            array = realloc(array, array_cap * sizeof(char*));
+            if (array == NULL) {
+                return NULL;
+            }
+        }
+        array[array_len] = NULL;
+        if (getline(array + array_len, &str_size, file) == -1) {
+            if (errno != 0) {
+                if (array != NULL) {
+                    for (int i = 0; i < array_len; i++) {
+                        free(array[i]);
+                    }
+                    free(array);
+                }
+                errno = old_errno;
+                return NULL;
+            } else {
+                break;
+            }
+        }
+        array_len++;
+    } while (1);
+    if (feof(file)) {
+        array[array_len] = NULL;
+        return array;
+    } else { // Some error processing the file
+        if (array != NULL) {
+            for (int i = 0; i < array_len; i++) {
+                free(array[i]);
+            }
+            free(array);
+        }
+        return NULL;
+    }
 }
 
 int load_ints(FILE* file, int** pointer) {
---
+++ c/src/day02.c
@@ -0,0 +1,105 @@
+#include <stdio.h>
+#include "lib.h"
+
+typedef struct Position {
+    int position;
+    int depth;
+    int aim;
+} Position;
+
+typedef struct Command {
+    char type;
+    int magnitude;
+} Command;
+
+Command parse_command(char* str) {
+    Command command;
+    if (str != NULL) {
+        command.type = *str;
+        while (*str++ != ' ');
+        command.magnitude = atoi(str);
+    }
+    return command;
+}
+
+int solution1(char** array) {
+    Position position;
+    position.position = 0;
+    position.depth = 0;
+    position.aim = 0; // Unessary, but for clean code
+    while (*array != NULL) {
+        Command command = parse_command(*array);
+        switch (command.type) {
+            case 'f':
+                position.position += command.magnitude;
+                break;
+            case 'd':
+                position.depth += command.magnitude;
+                break;
+            case 'u':
+                position.depth -= command.magnitude;
+                break;
+            default:
+                return -1;
+        }
+        array++;
+    }
+    return position.position * position.depth;
+}
+
+int solution2(char** array) {
+    Position position;
+    position.position = 0;
+    position.depth = 0;
+    position.aim = 0;
+    while (*array != NULL) {
+        Command command = parse_command(*array);
+        switch (command.type) {
+            case 'f':
+                position.position += command.magnitude;
+                position.depth += position.aim * command.magnitude;
+                break;
+            case 'd':
+                position.aim += command.magnitude;
+                break;
+            case 'u':
+                position.aim -= command.magnitude;
+                break;
+            default:
+                return -1;
+        }
+        array++;
+    }
+    return position.position * position.depth;
+}
+
+int main(int argc, char *args[]) {
+    char* test_filename = get_test_input_filename(args);
+    FILE* test_file = fopen(test_filename, "r");
+    if (test_file == NULL) {
+        return -1;
+    }
+    char** test_array = load_strs(test_file);
+    if (test_array == NULL) {
+        return -1;
+    }
+    fclose(test_file);
+    printf("%d\n", solution1(test_array));
+    printf("%d\n", solution2(test_array));
+    free_str_array(test_array);
+
+    char* filename = get_input_filename(args);
+    FILE* file = fopen(filename, "r");
+    if (file == NULL) {
+        return -1;
+    }
+    char** array = load_strs(file);
+    if (array == NULL) {
+        return -1;
+    }
+    fclose(file);
+    printf("%d\n", solution1(array));
+    printf("%d\n", solution2(array));
+    free_str_array(test_array);
+    return 0;
+}
